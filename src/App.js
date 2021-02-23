b8132c2c71741d640a6f786d206ca2167e1ef0dc
Hramchenlo Alexey <qwefix@gmail.com>
2021-02-23 13:44:47 +0300

    feat: footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 function App() {
-  return 
+  return (
+    'hi'
+  )
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import RSSLogo from './assetes/rs_school_js.svg'
+
+function Footer() {
+    return (
+        <footer>
+            <div className = 'logo-rss'>
+                <a href = 'https://rs.school/js/'>
+                    <img src = {RSSLogo} alt='Roling Scopes School' height = '30'/>
+                </a>
+            </div>
+            <div className = 'year'>
+                2021
+            </div>
+            <div className = 'github'>
+                <a href = 'https://github.com/qwefix'>qwefix</a>
+            </div>
+        </footer>
+    )
+}
+export default Footer
